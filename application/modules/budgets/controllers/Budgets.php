f548d11fa0968f8f2b98cbaffb8b4d89b64dcab8
Md Shahajahan Ali <msacse1@gmail.com>
2024-02-20 17:05:37 +0600

    budget module create

--- application/modules/budgets/controllers/Budgets.php
+++ application/modules/budgets/controllers/Budgets.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Budget extends Backend_Controller {
+class Budgets extends Backend_Controller {
 
 	public function __construct(){
       parent::__construct();
--- application/modules/budgets/models/Budgets_model.php
+++ application/modules/budgets/models/Budgets_model.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Budget_model extends CI_Model {
+class Budgets_model extends CI_Model {
 
     public function __construct() {
         parent::__construct();
