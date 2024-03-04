eca2ebe6df16f6fa33fd00da783871de2554bf51
Ahaduzzaman <75909816+ahaduzzamanapon@users.noreply.github.com>
2024-03-04 19:16:11 +0600

    Some Changes From Apon

--- application/modules/budgets/views/budget_field/print.php
+++ application/modules/budgets/views/budget_field/print.php
@@ -40,7 +40,8 @@
             td, th {
                 border: 1px solid black;
                 text-align: left;
-                padding: 8px;
+                padding: 4px;
+                font-size: 12px;
             }
             th {
                 text-align: center;
@@ -56,28 +57,28 @@
         </style>
     </head>
     <body style="font-family: DejaVu Sans; font-size: 12px; line-height: 1.5 !important;">
-        <div class="heading">
+        <div class="heading" >
             <div class="col-md-12">
                 <div class="row">
                     <div class="col-md-3" style="display: flex;flex-wrap: wrap;align-content: center;">
                         <?php
                         $url = base_url('awedget/assets/img/nilg-logo.png');
                         ?>
-                        <div style="display: flex;flex-wrap: wrap;align-content: center;justify-content: center;align-items: center;height: 120px;">
+                        <div style="display: flex;flex-wrap: wrap;align-content: center;justify-content: center;align-items: center;height: 80px;">
                             <img style="height: 100%;width: 100%;" src="<?=$url?>" alt="logo">
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <h5 class="text-center">
-                            <span style="font-size:14px;">গণপ্রজাতন্ত্রী বাংলাদেশ সরকার</span><br>
-                            <span style="font-size:14px;">স্থানীয় সরকার, পল্লী উন্নয়ন ও সমবায় মন্ত্রণালয়</span><br>
-                            <span style="font-size:15px; font-weight: bold;">জাতীয় স্থানীয় সরকার ইনস্টিটিউট (এনআইএলজি )</span><br>
-                            <span style="font-size:13px;">২৯, আগারগাঁও, শেরে বাংলা নগর, ঢাকা - ১২০৭ </span><br>
-                            <span style="font-size:13px;text-decoration: underline;color: #2246ff;">www.nilg.gov.bd </span>
+                        <h5 class="text-center" style="padding: 0;margin-bottom: 0;">
+                            <span style="font-size:13px;">গণপ্রজাতন্ত্রী বাংলাদেশ সরকার</span><br>
+                            <span style="font-size:13px;">স্থানীয় সরকার, পল্লী উন্নয়ন ও সমবায় মন্ত্রণালয়</span><br>
+                            <span style="font-size:14px; font-weight: bold;">জাতীয় স্থানীয় সরকার ইনস্টিটিউট (এনআইএলজি )</span><br>
+                            <span style="font-size:12px;">২৯, আগারগাঁও, শেরে বাংলা নগর, ঢাকা - ১২০৭ </span><br>
+                            <span style="font-size:12px;text-decoration: underline;color: #2246ff;">www.nilg.gov.bd </span>
                         </h5>
                     </div>
                     <div class="col-md-3"style="display: flex;align-content: center;flex-wrap: wrap;right: 0;position: relative;justify-content: flex-end;">
-                        <div style="margin-top: 14px; padding: 4px; border: 2px solid; font-size: 15px; float: right;">
+                        <div style="padding: 4px; border: 2px solid; font-size: 13px; float: right;">
                             <span> শেখ হাসিনার মূলনীতি </span> <br>
                             <span> গ্রাম শহরের উন্নতি </span>
                         </div>
@@ -85,7 +86,7 @@
                 </div>
             </div>
         </div>
-        <div class="heading_footer">
+        <div class="heading_footer" style="height: 44px;">
             <div class="col-md-12" style="display: flex;flex-wrap: wrap;align-content: center;justify-content: center;align-items: center;">
                 <h4><span> বাজেটঃ <?=$budget_field->title;?></span></h4>
             </div>
@@ -143,8 +144,15 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.1/html2pdf.bundle.min.js"></script>
         <script>
             window.onload = function() {
-
-                html2pdf(document.body);
+                html2pdf(document.body, {
+                    margin: 10,
+                    filename: 'document.pdf',
+                    image: { type: 'jpeg', quality: 0.98 },
+                    html2canvas: { scale: 3 },
+                    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+                }).then(function (pdf) {
+                    window.close();
+                });
             }
         </script>
     </body>
