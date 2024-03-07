b0407aa8f0cf3f81c584b7cb934f1e539811a851
Md Shahajahan Ali <msacse1@gmail.com>
2024-03-07 12:59:31 +0600

    Some Modified

--- application/modules/budgets/views/budget_nilg/index.php
+++ application/modules/budgets/views/budget_nilg/index.php
@@ -12,7 +12,8 @@
                <div class="grid-title">
                   <h4><span class="semi-bold"><?=$meta_title; ?></span></h4>
                   <div class="pull-right">
-                     <a class="btn btn-info btn-xs btn-mini" href="<?php echo base_url('budgets/budget_nilg_summary') ?>" target="_blank"><i class="fa fa-book"></i> সামারী করুন </a>
+                     <a class="btn btn-success btn-xs btn-mini" href="<?php echo base_url('budgets/nilg_revenue_summary') ?>" target="_blank"><i class="fa fa-book"></i> রিভিনিও সামারী  </a>
+                     <a class="btn btn-info btn-xs btn-mini" href="<?php echo base_url('budgets/nilg_acc_summary') ?>" target="_blank"><i class="fa fa-book"></i> সামারী করুন </a>
                      <a href="<?=base_url('budgets/budget_nilg_create')?>" class="btn btn-blueviolet btn-xs btn-mini"> Create Budget</a>
                   </div>
                </div>
