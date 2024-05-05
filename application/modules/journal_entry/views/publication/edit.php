0b6664d68ce9fe04aafc4863c21f912067d114a6
Md Shahajahan Ali <msacse1@gmail.com>
2024-05-05 15:00:10 +0600

    Some Modified

--- application/modules/journal_entry/views/publication/edit.php
+++ application/modules/journal_entry/views/publication/edit.php
@@ -42,51 +42,81 @@
                         </div>
                         <?php endif; ?>
 
-                        <?php
-                            $attributes = array('id' => 'jsvalidate');
+                        <?php $attributes = array('id' => 'jsvalidate');
                             echo form_open_multipart("journal_entry/publication_entry_edit",$attributes);
-                            echo validation_errors();
-                        ?>
+                            echo validation_errors(); ?>
 
-                        <input type="hidden" name="id" value="<?=$budget_j_publication_register->id?>">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <fieldset style="background: #fff !important;">
-                                    <legend>পাবলিকেশন তথ্য</legend>
-                                    <div class="row form-row" style="font-size: 16px; color: black; margin-top: -20px !important;">
-                                        <br>
-                                        <div class="col-md-3">
-                                            <label for="title" class="control-label">ভাউচার নাঃ </label> <span> <?= $budget_j_publication_register->voucher_no ?></span>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <label for="title" class="control-label">রেফারেন্স:</label>
-                                            <input type="text"  value="<?=$budget_j_publication_register->reference?>" class="form-control input-sm" name="reference"
-                                                style="min-height: 33px;">
-                                        </div>
-                                        <div class="col-md-3">
-                                            <label for="title" class="control-label">তারিখ:</label>
-                                            <input type="date"  value="<?=$budget_j_publication_register->issue_date?>" class="form-control input-sm" name="issue_date"
-                                                style="min-height: 33px;" required>
-                                        </div>
-                                        <div class="col-md-3">
-                                            <label for="title" class="control-label"> পরিমান:</label>
-                                            <input type="number"  value="<?=$budget_j_publication_register->amount?>" class="form-control input-sm" name="amount"
-                                                style="min-height: 33px;" required>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <label for="title" class="control-label">বর্ণনা:</label>
-                                            <textarea name="description" id="" style="width: 100%; height: 85px;"><?=$budget_j_publication_register->description?></textarea>
-                                        </div>
+                            <input type="hidden" name="id" value="<?=$budget_j_publication_register->id?>">
+                            <div class="row form-row" style="font-size: 16px; color: black; margin-top: -20px !important;">
+                                <br>
+                                <div class="col-md-2">
+                                    <label for="title" class="control-label">ভাউচার নাঃ </label> <span> <?= $budget_j_publication_register->voucher_no ?></span>
+                                </div>
 
+                                <div class="col-md-4">
+                                    <label for="title" class="control-label">বই নাম:</label>
+                                    <input type="text"  value="<?=$budget_j_publication_register->book_name?>" class="form-control input-sm" name="book_name" style="min-height: 33px;">
+                                </div>
+                                <div class="col-md-3">
+                                    <label for="title" class="control-label"> এসবিএন নং</label>
+                                    <input type="text"  value="<?=$budget_j_publication_register->sbn_no?>" class="form-control input-sm" name="sbn_no"
+                                        style="min-height: 33px;">
+                                </div>
+                                <div class="col-md-3">
+                                    <label for="title" class="control-label">রেফারেন্স:</label>
+                                    <input type="text"  value="<?=$budget_j_publication_register->reference?>"  class="form-control input-sm" name="reference"
+                                        style="min-height: 33px;">
+                                </div>
+                            </div>
+
+                            <div class="row form-row" style="font-size: 16px; color: black; margin-top: -20px !important;">
+                                <br>
+                                <div class="col-md-3">
+                                    <label for="title"  class="control-label"> বইয়ের মূল্য: <span class="required">*</span></label>
+                                    <input type="number" id="price" onkeyup="cal_book_amt()" value="<?=$budget_j_publication_register->price?>" class="form-control input-sm" name="price"
+                                        style="min-height: 33px;" required>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <label for="title" class="control-label"> পরিমান: <span class="required">*</span></label>
+                                    <input type="number" id="quantity" onkeyup="cal_book_amt()" value="<?=$budget_j_publication_register->quantity?>"  class="form-control input-sm" name="quantity"
+                                        style="min-height: 33px;" required>
+                                </div>
+
+                                <div class="col-md-3">
+                                    <label for="title" class="control-label"> মোট মূল্য: <span class="required">*</span></label>
+                                    <input type="number" id="amount" value="<?=$budget_j_publication_register->amount?>" class="form-control input-sm" name="amount"
+                                        style="min-height: 33px;" required readonly>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <label for="title" class="control-label">তারিখ: <span class="required">*</span></label>
+                                    <input type="date"  value="<?=$budget_j_publication_register->issue_date?>" class="form-control input-sm" name="issue_date"
+                                        style="min-height: 33px;" required>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <label for="title" class="control-label"> ধরণ: <span class="required">*</span></label>
+                                    <select name="type" id="" class="form-control input-sm" required>
+                                        <option value=""> Select Type</option>
+                                        <option value=1>Cash Deposit</option>
+                                        <option value=2>Payment Voucher</option>
+                                        <option value="3">Adjustment Voucher</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-12">
+                                    <label for="title" class="control-label">বর্ণনা:</label>
+                                    <textarea class="form-control input-sm" name="description" ><?=$budget_j_publication_register->description?></textarea>
+                                </div>
+
+                                <div class="col-md-12">
+                                    <br>
+                                    <div class="pull-right">
+                                        <input type="submit" name="submit" id="submit_btn" value="সংরক্ষণ করুন" class="btn btn-primary btn-cons">
                                     </div>
-                                </fieldset>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-actions">
-                            <div class="pull-right">
-                                <input type="submit" name="submit" id="submit_btn" value="সংরক্ষণ করুন" class="btn btn-primary btn-cons">
-                            </div>
-                        </div>
                         <?php echo form_close();?>
                     </div> <!-- END GRID BODY -->
                 </div> <!-- END GRID -->
