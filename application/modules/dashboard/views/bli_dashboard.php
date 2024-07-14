f638023ecd9813d4845f7e7b09f212795cda9d05
Ahaduzzaman <75909816+ahaduzzamanapon@users.noreply.github.com>
2024-07-14 16:29:02 +0600

    Some Changes From Apon

--- application/modules/dashboard/views/bli_dashboard.php
+++ application/modules/dashboard/views/bli_dashboard.php
@@ -1,105 +1,151 @@
+<?php 
+    $query = $this->db->select('
+                        SUM(CASE WHEN type = 1 THEN quantity ELSE 0 END) AS total_in,
+                        SUM(CASE WHEN type = 2 THEN quantity ELSE 0 END) AS total_sale,
+                        SUM(CASE WHEN type = 3 THEN quantity ELSE 0 END) AS total_gift,
+                        SUM(CASE WHEN type = 4 THEN quantity ELSE 0 END) AS total_sell_by_kg,
 
+                        SUM(CASE WHEN type = 1 THEN amount ELSE 0 END) AS total_in_amount,
+                        SUM(CASE WHEN type = 2 THEN amount ELSE 0 END) AS total_sale_amount,
+                        SUM(CASE WHEN type = 3 THEN amount ELSE 0 END) AS total_gift_amount,
+                        SUM(CASE WHEN type = 4 THEN amount ELSE 0 END) AS total_sell_by_kg_amount
+                    ')
+                    ->get('budget_j_publication_register_details')
+                    ->row();
+    //book_card info 
+    $total_in = $query->total_in;
+    $total_sale = $query->total_sale;
+    $total_gift = $query->total_gift;
+    $total_sell_by_kg = $query->total_sell_by_kg;
+    $balance = $total_in - ($total_sale + $total_gift + $total_sell_by_kg);
 
+    //book_amont_card info
+    $total_in_amount = $query->total_in_amount;
+    $total_sale_amount = $query->total_sale_amount;
+    $total_gift_amount = $query->total_gift_amount;
+    $total_sell_by_kg_amount = $query->total_sell_by_kg_amount;
+    $balance_amount = $total_in_amount - ($total_sale_amount + $total_gift_amount + $total_sell_by_kg_amount);
+
+?>
+<style>
+.card {
+    background-color: white;
+    color: black;
+    border-radius: 5px;
+    padding: 20px;
+    margin: 20px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card-header {
+    font-size: 1.7em;
+    font-weight: bold;
+    text-align: left;
+    margin-bottom: 20px;
+    padding-left: 4px;
+    border-bottom: 2px solid;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+th,
+td {
+    padding: 10px;
+    text-align: left;
+}
+th{
+    font-size: 14px;
+    font-weight: bolder;
+}
+
+.card-container {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
 <div class="page-content">
-
     <div class="content">
-    <?php 
-      $user = $this->ion_auth->user()->row();
-      $dept_id = $user->crrnt_dept_id;
-      $this->db->select('
-      SUM(CASE WHEN status = 1 THEN amount ELSE 0 END) AS total_amount,
-      ');
-      $this->db->where('dept_id', $dept_id);
-      $total_amount = $this->db->get('budget_field')->row();
-
-      $this->db->select('
-      SUM(CASE WHEN type = 1 THEN amount ELSE 0 END) AS total_amount,
-      SUM(CASE WHEN type = 1 THEN 1 ELSE 0 END) AS count,
-      ');
-      $total_amount_ay = $this->db->get('budget_j_publication_register')->row();
-
-
-
-
-
-
-
-
-
-
-    ?>
         <div class="row">
             <div class="col-md-12">
-                <h1 style="text-align: center; color: #333;">স্বাগতম, এনআইএলজি (এমআইএস) | জাতীয় স্থানীয় সরকার ইনস্টিটিউট</h1>
-                <!-- <img src="<?=base_url('awedget/assets/img/nilg-logo.png')?>" width="50" style="float: left; margin-right: 20px;"> -->
+                <h1 style="text-align: left; color: #333;">স্বাগতম, এনআইএলজি (এমআইএস) | জাতীয় স্থানীয় সরকার ইনস্টিটিউট
+                </h1>
             </div>
         </div>
         <div class="row">
-        <div class="col-md-4">
-                <div class="panel panel-default text-center"
-                    style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s; border: none; border-radius: 10px; background-color: #fff; padding: 20px;">
-                    <div class="panel-body">
-                        <h3 style="color: #53d769; font-weight: bold; font-size: 36px; margin-bottom: 10px;" id="incomeCounter">৳ <?=($total_amount_ay->total_amount!='')?eng2bng($total_amount_ay->total_amount):'০.০০'?></h3>
-                        <h4 style="color: #555; font-size: 20px; margin-bottom: 0;">মোট আয় </h4>
-                    </div>
+            <div class="card-container col-md-12">
+                <div class="card col-md-6">
+                    <div class="card-header">বই এর হিসাব</div>
+                    <table>
+                        <tr>
+                            <th>মোট প্রাপ্তি</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($total_in); ?></td>
+                        </tr>
+                        <tr>
+                            <th>মোট বিক্রয়</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($total_sale); ?></td>
+                        </tr>
+                        <tr>
+                            <th>মোট উপহার</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($total_gift); ?></td>
+                        </tr>
+                        <tr>
+                            <th>কেজি দ্বারা বিক্রয়</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($total_sell_by_kg); ?></td>
+                        </tr>
+                        <tr>
+                            <th>ব্যালান্স</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($balance); ?></td>
+                        </tr>
+                    </table>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-default text-center"
-                    style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s; border: none; border-radius: 10px; background-color: #fff; padding: 20px;">
-                    <div class="panel-body">
-                        <h3 style="color: #ff6f61; font-weight: bold; font-size: 36px; margin-bottom: 10px;" id="expenseCounter">৳ <?=($total_amount->total_amount!='')?eng2bng($total_amount->total_amount):'০.০০'?></h3>
-                        <h4 style="color: #555; font-size: 20px; margin-bottom: 0;">মোট খরচ </h4>
-                    </div>
-                </div>
-            </div>
-          
-            <div class="col-md-4">
-                <div class="panel panel-default text-center"
-                    style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); transition: 0.3s; border: none; border-radius: 10px; background-color: #fff; padding: 20px;">
-                    <div class="panel-body">
-                        <h3 style="color: #4a90e2; font-weight: bold; font-size: 36px; margin-bottom: 10px;" id="entryCounter"><?=($total_amount_ay->count!='')?eng2bng($total_amount_ay->count):'০.০০'?></h3>
-                        <h4 style="color: #555; font-size: 20px; margin-bottom: 0;">মোট প্রকাশনা রেজিস্টার এন্ট্রি</h4>
-                    </div>
+                <div class="card col-md-6">
+                    <div class="card-header">অর্থের হিসাব</div>
+                    <table>
+                        <tr>
+                            <th>মোট প্রাপ্তি</th>
+                            <td>:</td>
+                            <td><?php echo eng2bng($total_in_amount); ?></td>
+                        </tr>
+                        <tr>
+                            <th>মোট বিক্রয়</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($total_sale_amount); ?></td>
+                        </tr>
+                        <tr>
+                            <th>মোট উপহার</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($total_gift_amount); ?></td>
+                        </tr>
+                        <tr>
+                            <th>কেজি দ্বারা বিক্রয়</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($total_sell_by_kg_amount); ?></td>
+                        </tr>
+                        <tr>
+                            <th>ব্যালান্স</th>
+                            <td>:</td>
+
+                            <td><?php echo eng2bng($balance_amount); ?></td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-<style>
-    .panel.panel-default:hover {
-        transform: scale(1.05);
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    }
-
-    .panel.panel-default:hover .panel-body {
-        background-color: #f5f5f5;
-    }
-
-    .panel-body {
-        transition: background-color 0.3s ease-in-out;
-    }
-</style>
-<!-- <script>
-    function animateValue(id, start, end, duration) {
-        if (start === end) return;
-        var range = end - start;
-        var current = start;
-        var increment = end > start ? 1 : -1;
-        var stepTime = Math.abs(Math.floor(duration / range));
-        var obj = document.getElementById(id);
-        var timer = setInterval(function() {
-            current += increment;
-            obj.textContent = current;
-            if (current == end) {
-                clearInterval(timer);
-            }
-        }, stepTime);
-    }
-
-    animateValue("expenseCounter", 0, 1000, 300); // Replace 1000 with the desired end value
-    animateValue("incomeCounter", 0, 3000, 300); // Replace 3000 with the desired end value
-    animateValue("entryCounter", 0, 10, 300); // Replace 10 with the desired end value
-</script> -->
-    
